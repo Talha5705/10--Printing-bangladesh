8df7cd6aa0f2c3497eca1815a2f8c308f8954f30
Talha5705 <ztalha@gmail.com>
2018-10-12 21:23:34 -0700

    first commit

---
+++ src/pkg10/printing/bangladesh/PrintingBangladesh.java
@@ -0,0 +1,61 @@
+
+package digitspelling;
+import java.util.Scanner;
+
+public class DigitSpelling {
+
+    int digit;
+    public static void main(String[] args) {
+        
+        System.out.println("Enter a digit to see the spelling");
+        Scanner sc=new Scanner(System.in);
+        
+        int digit=sc.nextInt();
+    
+        switch(digit){
+        
+            case 0:
+                System.out.println("Zero");
+                break;
+                
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            case 10:
+                System.out.println("Ten");
+                break;
+                
+            default :
+                    System.out.println("Invalid Input");
+        
+        }
+        
+        
+        
+    }
+    
+}
